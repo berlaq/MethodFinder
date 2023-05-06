fe7f92afb0d1ff7b749bd3ed37e61035ceb90843
Berkay Haberal <haberal54@gmail.com>
2023-05-06 18:02:38 +0300

    INITIAL COMMIT
    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.7.0"
+}
+
+group = "com.methodFinder"
+version = "1.2-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("com.intellij.java"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("201")
+        untilBuild.set("231.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("TOKEN"))
+    }
+}
---
+++ src/main/java/com/methodfinder/methodfinder/MethodFinder.java
@@ -0,0 +1,80 @@
+package com.methodfinder.methodfinder;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MethodFinder extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        String className = findClassName(e);
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        Project project = e.getProject();
+        assert editor != null;
+        CaretModel caretModel = editor.getCaretModel();
+        LogicalPosition initialPosition = caretModel.getLogicalPosition();
+
+        assert project != null;
+        PsiClass psiClass = PsiShortNamesCache.getInstance(project).getClassesByName(className, GlobalSearchScope.everythingScope(project))[0];
+        Map<String, PsiMethod> methods = Arrays.stream(psiClass.getMethods())
+                .filter(method -> method.getModifierList().hasModifierProperty(PsiModifier.PUBLIC))
+                .collect(Collectors.toMap(PsiMethod::getName, method -> method));
+
+        JBPopupFactory popupFactory = JBPopupFactory.getInstance();
+        popupFactory.createPopupChooserBuilder(new ArrayList<>(methods.keySet()))
+                .setTitle("Available Public Methods")
+                .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+                .setItemChosenCallback((chosen) -> WriteCommandAction.runWriteCommandAction(project, () -> {
+                    String sentence = buildSentence(methods.get(chosen));
+                    editor.getDocument().insertString(caretModel.getOffset(), sentence);
+                    LogicalPosition logicalPosition = new LogicalPosition(initialPosition.line, initialPosition.column + sentence.length());
+                    CaretState caretState = new CaretState(logicalPosition, null, null);
+                    editor.getCaretModel().setCaretsAndSelections(List.of(caretState));
+                }))
+                .createPopup()
+                .showInBestPositionFor(e.getDataContext());
+    }
+
+    private String buildSentence(PsiMethod method) {
+        return '"' + method.getName() + '"' +
+                ", " + buildReturnType(method);
+    }
+
+    private String buildReturnType(PsiMethod method) {
+        String presentableText = Objects.requireNonNull(method.getReturnType()).getPresentableText();
+        if (presentableText.contains("<")) {
+            return presentableText.substring(0, presentableText.indexOf("<"))+".class, ";
+        } else {
+            return presentableText+".class,".trim()+" ";
+        }
+    }
+
+    private String findClassName(AnActionEvent event) {
+        Editor editor = PlatformDataKeys.EDITOR.getData(event.getDataContext());
+        assert editor != null;
+        CaretModel caretModel = editor.getCaretModel();
+        LogicalPosition logicalPosition = caretModel.getLogicalPosition();
+
+        Document document = editor.getDocument();
+        int lineStartOffset = document.getLineStartOffset(logicalPosition.line);
+        int lineEndOffset = document.getLineEndOffset(logicalPosition.line);
+
+        TextRange textRange = new TextRange(lineStartOffset, lineEndOffset);
+        String text = document.getText(textRange);
+        return text.substring(text.lastIndexOf("(", logicalPosition.column)+1, text.lastIndexOf(".", logicalPosition.column));
+    }
+}
